d2bb690c7610fbb8bacb3db5c07a462a9bf88c52
CookieDragon16 <radoliub.milencovici@student.upt.ro>
2022-05-24 01:22:28 +0300

    Updated UserService Added Exception handling for registration Added javafx dependency for pom.xml Created RegistrationController

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
-
+import controllers.RegistrationController;
+import javafx.scene.Parent;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("Hello Word!");
+
+
     }
 }
---
+++ src/main/java/exceptions/PasswordsMisatchException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class PasswordsMisatchException extends Exception{
+}
