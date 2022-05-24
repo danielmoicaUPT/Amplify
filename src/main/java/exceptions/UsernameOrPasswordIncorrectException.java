8464fd56c1fb0a5df9a287fafcce01098723a408
CookieDragon16 <radoliub.milencovici@student.upt.ro>
2022-05-24 23:44:56 +0300

    Check user in database now returns different errors for non-existent user/ wrong password

--- src/main/java/exceptions/UsernameOrPasswordIncorrectException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class UsernameOrPasswordIncorrectException extends Exception{
-    public UsernameOrPasswordIncorrectException() {
-        super(String.format("Username or password don't match with any users"));
-    }
-}
